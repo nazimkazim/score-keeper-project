f4b1a9d608c768ea04c2356305549896c386c067
Your Name <nazik_111@mail.ru>
2018-08-23 18:47:13 +0500

    add mongoDB

--- client/main.js
+++ client/main.js
@@ -2,8 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Meteor } from 'meteor/meteor';
 
+const players = [
+  {
+    _id: '1',
+    name: 'Lauren',
+    score: 99
+  },
+  {
+    _id: '2',
+    name: 'Cory',
+    score: -1
+  },
+  {
+    _id: '3',
+    name: 'Andrew',
+    score: -12
+  }
+];
+
+const renderPlayers = function(playersList) {
+  return playersList.map(function(player) {
+    return (
+      <p key={player._id}>
+        {player.name} has {player.score} point(s)
+      </p>
+    );
+  });
+};
+
 Meteor.startup(function() {
-  let name = 'Serik';
-  let jsx = <p>Hello {name}!</p>;
-  ReactDOM.render(jsx, app);
+  let title = 'Score keeper app';
+  let name = 'Nazim';
+  let jsx = (
+    <div>
+      <h1>{title}</h1>
+      <p>Hello {name}</p>
+      {renderPlayers(players)}
+    </div>
+  );
+  ReactDOM.render(jsx, document.getElementById('app'));
 });
--- server/main.js
+++ server/main.js
@@ -1,5 +1,10 @@
 import { Meteor } from 'meteor/meteor';
+import { Players } from './../imports/api/players';
 
-Meteor.startup(() => {
-  // code to run on server at startup
+Meteor.startup(function() {
+  Players.insert({
+    name: 'Rasul',
+    score: 120
+  });
+  console.log(Players.find().fetch());
 });
