1af1913a0aba1e7423552774cb3c2e5c4caf0637
Your Name <nazik_111@mail.ru>
2018-08-30 13:17:37 +0500

    add mongo sort

--- client/main.js
+++ client/main.js
@@ -8,7 +8,15 @@ import App from './../imports/ui/App';
 
 Meteor.startup(() => {
   Tracker.autorun(() => {
-    let players = Players.find().fetch();
+    let players = Players.find(
+      {},
+      {
+        sort: {
+          score: -1,
+          name: 1
+        }
+      }
+    ).fetch();
     let title = 'Score keeper app';
 
     ReactDOM.render(
